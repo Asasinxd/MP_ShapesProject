3608b9317640e204838c528e5215af65d1d8ed92
Asasinxd <peter.saletra@gmail.com>
2023-05-28 18:51:06 +0200

    Structure of project

---
+++ Display/GrapicalDisplay.py
@@ -0,0 +1,11 @@
+from Display import Display
+
+class GrapicalDisplay(Display):
+    def drawTriangle(self, a1, a2, b1, b2):
+        pass
+
+    def drawCircle(self, r):
+        pass
+
+    def drawParallelogramm(self, a1, a2, b1, b2):
+        pass
